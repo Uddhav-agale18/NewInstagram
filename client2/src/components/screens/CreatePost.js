7ca081ab8b5e9d3d66728d15e3797d13ce770217
Uddhav <uddhav@gmail.com>
2022-12-22 13:45:42 +0530

    first commit

--- client2/src/components/screens/CreatePost.js
+++ client2/src/components/screens/CreatePost.js
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from "react";
 import {useNavigate} from 'react-router-dom'
 import M from 'materialize-css'
 
-
 const CreatePost =()=>{
   const navigate = useNavigate()   
 
